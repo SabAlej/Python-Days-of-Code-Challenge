32d49fb407578c71351cddf0d04614e63c683ad7
SabAlej <sabrinaacm@gmail.com>
2024-01-12 20:29:42 -0500

    fix lint

--- 2 - Day 2/solution.py
+++ 2 - Day 2/solution.py
@@ -1,12 +1,9 @@
 """
 This module contains a program that calculates the area of a circle given its radius.
 """
-
 import math
 
 radius = input("Radius: ")
-
 area = math.pi * float(radius) ** 2
 
-print("Area: " + str(area))
-
+print("Area of the circle: ",str(area))
